1d668db537c530cf3a8af6f1399e577e015b0761
jmateos2021 <jmateos2021@gmail.com>
2025-10-28 19:17:56 +0100

    COMMIT INITIAL

---
+++ notes/src/main/java/es/notes/notes/model/Note.java
@@ -0,0 +1,50 @@
+package es.notes.notes.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Note {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+    private String content;
+    private boolean completed;
+
+    public boolean isCompleted() {
+        return completed;
+    }
+
+    public void setCompleted(boolean completed) {
+        this.completed = completed;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
---
+++ notes/src/main/java/es/notes/notes/service/NoteService.java
@@ -0,0 +1,7 @@
+package es.notes.notes.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class NoteService {
+}
